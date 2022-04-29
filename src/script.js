b67310e449bacd327761042de42395bdeba8a50f
Barbara Carmack <barb@carmackws.com>
2022-04-29 14:38:14 -0600

    Added individual elements

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ function displayTemperature(response)  {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML=Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
